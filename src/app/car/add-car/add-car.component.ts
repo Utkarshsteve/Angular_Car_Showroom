6334e1e3b5d81013139fc3bfcf5c4b85fa1ccb4a
Utkarshsteve <utkarshsteve7@gmail.com>
2021-05-10 20:00:21 +0530

    Added Template Driven Form

--- src/app/car/add-car/add-car.component.ts
+++ src/app/car/add-car/add-car.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +9,7 @@ import { Router } from '@angular/router';
 })
 export class AddCarComponent implements OnInit {
 
+  @ViewChild('Form') addCarForm: NgForm;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -16,6 +18,11 @@ export class AddCarComponent implements OnInit {
   onBack()
   {
     this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log("Submit Works");
+    console.log(this.addCarForm);
   }
 
 }
