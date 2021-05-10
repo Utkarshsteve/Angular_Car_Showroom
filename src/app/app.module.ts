77308372986c9a6e8eb1827a14421285f15368e2
Utkarshsteve <utkarshsteve7@gmail.com>
2021-05-10 22:20:43 +0530

    Added Reactive Forms(Register) with custom Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +12,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SportscarsService } from 'src/services/sportscars.service';
 import { AddCarComponent } from './car/add-car/add-car.component';
 import { CarDetailComponent } from './car/car-detail/car-detail.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
 const appRoutes: Routes = [
   
@@ -19,6 +21,8 @@ const appRoutes: Routes = [
   {path: 'rent-car', component: CarListComponent},
   {path: 'add-car', component: AddCarComponent},
   {path: 'car-detail/:id', component: CarDetailComponent},
+  {path: 'user/login', component: UserLoginComponent},
+  {path: 'user/register', component: UserRegisterComponent},
   {path: '**', component: CarListComponent}
 
 
@@ -31,12 +35,15 @@ const appRoutes: Routes = [
     CarListComponent,
     NavBarComponent,
     AddCarComponent,
-    CarDetailComponent
+    CarDetailComponent,
+    UserLoginComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
